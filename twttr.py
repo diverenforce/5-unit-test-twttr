b3e3636a1bbf28a051a5a5bae06db15e83e8973b
enforcer <120410051+diverenforce@users.noreply.github.com>
2024-08-07 21:35:30 +0630

    Final touch

--- twttr.py
+++ twttr.py
@@ -1,19 +1,23 @@
 def main():
     word = input('Input: ').strip()
-    print(f'Output: {shorten(word)}')
+    print(f'Output: {shorten(lstmaker(word))}')
 
 
-def shorten(word):
-    lst = []
-    for aletter in range(len(word)):
-        lst.append(word[aletter])
+def shorten(lst):
 
     for letters in range(len(lst)):
         match lst[letters]:
             case 'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U':
-                lst[letters] = lst[letters].replace(lst[letters][0],'')
+                lst[letters] = lst[letters].replace(lst[letters],'')
     
     return ''.join(lst)
+
+def lstmaker(word):
+    lst = []
+    for aletter in range(len(word)):
+        lst.append(word[aletter])
+    return lst
+
 
 if __name__ == "__main__":
     main()
