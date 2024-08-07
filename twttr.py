101dfc41ab5e455073b5a5efef59434ad5b21452
enforcer <120410051+diverenforce@users.noreply.github.com>
2024-08-07 21:30:18 +0630

    Replace badletters and join lst

--- twttr.py
+++ twttr.py
@@ -8,6 +8,12 @@ def shorten(word):
     for aletter in range(len(word)):
         lst.append(word[aletter])
 
+    for letters in range(len(lst)):
+        match lst[letters]:
+            case 'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U':
+                lst[letters] = lst[letters].replace(lst[letters][0],'')
+    
+    return ''.join(lst)
 
 if __name__ == "__main__":
     main()
